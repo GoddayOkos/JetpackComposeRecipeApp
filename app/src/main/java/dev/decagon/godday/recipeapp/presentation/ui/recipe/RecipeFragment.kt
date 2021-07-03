afecb4b8aa219b0b1febd65d51ff750670323fe9
Godday Okoduwa <goddayokos@gmail.com>
2021-07-03 17:17:24 +0100

    Started implementation of RecipeFragment

--- app/src/main/java/dev/decagon/godday/recipeapp/presentation/ui/recipe/RecipeFragment.kt
+++ app/src/main/java/dev/decagon/godday/recipeapp/presentation/ui/recipe/RecipeFragment.kt
@@ -1,7 +1,51 @@
 package dev.decagon.godday.recipeapp.presentation.ui.recipe
 
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.Text
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.ComposeView
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.navArgs
 import dagger.hilt.android.AndroidEntryPoint
+import java.nio.file.WatchEvent
 
 @AndroidEntryPoint
-class RecipeFragment : Fragment()
+class RecipeFragment : Fragment() {
+    private val args: RecipeFragmentArgs by navArgs()
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        return ComposeView(requireContext()).apply {
+            val recipeId = args.recipeId
+            setContent {
+                Column(
+                    modifier = Modifier
+                        .padding(16.dp)
+                        .fillMaxSize(),
+                    verticalArrangement = Arrangement.Center,
+                    horizontalAlignment = Alignment.CenterHorizontally
+                ) {
+                    Text(
+                        text = "Selected recipeId: $recipeId",
+                        color = Color.Green,
+                        fontSize = 21.sp
+                    )
+                }
+            }
+        }
+    }
+}
